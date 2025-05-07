fd79a555a8d47fcaeef6e5198195ae9d03afde29
Clayton Singh <claytonsingh@gmail.com>
2025-05-06 22:20:30 -0700

    Initial Commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+# Install build tools
+pip install -r requirements-dev.txt
+
+# Build the package
+if [ "$(python -c 'import sys; print(sys.version_info[0])')" = "2" ]; then
+    python -m build --no-isolation
+else
+    python -m build
+fi
+
+# Check and upload to PyPI
+# twine check dist/*
+# twine upload dist/*
